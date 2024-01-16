b3604eb5935e07b57ecae49821f3b61dd20e022c
Sergiu333 <anghelenicis59@gmail.com>
2024-01-16 11:12:29 +0200

    int header component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
 import Image from 'next/image'
+import Header from "@/components/Header";
 
 export default function Home() {
   return (
-    <div>pagina</div>
+    <div className="bg-hero h-[100vh] relative">
+      <Header/>
+    </div>
   )
 }
