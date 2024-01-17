c1631f20a03c64be7a32d50b31d13f9078d16db5
Sergiu333 <anghelenicis59@gmail.com>
2024-01-17 10:08:08 +0200

    create component hero and added font moniqa

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -3,7 +3,12 @@ import Image from "next/image";
 const Hero = () => {
   return(
       <div className="4xl:px-[90px] 4xl:pt-[40px] relative">
-          <hr className="border border-white/[.5] w-[914px] absolute left-0 top-[207px]"/>
+          <div className="absolute left-0 right-0 top-[207px] w-full">
+              <hr className="border border-white/[.5] w-[50%] mt-[34px] absolute left-0"/>
+              <div className="hero-text 3xl:pl-[511px]">Photography</div>
+              <hr className="border border-white/[.5] w-[60%] mt-[11px] absolute right-0"/>
+              <div className="hero-text_bottom absolute right-0 pt-[33px] 3xl:pr-[565px]">as Art</div>
+          </div>
           <div className="flex flex-row justify-between w-full">
               <Image src="/img-left-hero.jpg" alt="left image" width={498} height={711} />
               <div className="pt-[100px]">
