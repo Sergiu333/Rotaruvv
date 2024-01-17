c299a40b45e8142759399c2cc1846f522f5a22c4
Sergiu333 <anghelenicis59@gmail.com>
2024-01-17 10:20:11 +0200

    changed menu on mobile

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,14 +2,14 @@ import Image from "next/image";
 
 const Header = () => {
   return(
-      <div className="4xl:px-[344px] pt-[60px]">
+      <div className="px-5 xs:px-14 sm:px-24 md:px-32 lg:px-44 xl:px-48 2xl:px-56 3xl:px-[344px] pt-[60px]">
         <div className="flex flex-row items-center justify-between">
-            <div className="flex flex-row gap-[100px] text-lg text-[#FFFFFFB2]">
+            <div className="flex flex-row gap-[10px] lg:gap-[50px] 3xl:gap-[100px] text-lg text-[#FFFFFFB2] hidden md:flex">
                 <div>Portfolio</div>
                 <div>Info</div>
             </div>
             <Image src="/Rotaruvv-logo.svg" alt="" width={200} height={80} />
-            <div className="flex flex-row gap-[100px] text-lg text-[#FFFFFFB2]">
+            <div className="flex flex-row gap-[20px] lg:gap-[50px] 3xl:gap-[100px] text-lg text-[#FFFFFFB2] hidden md:flex">
                 <div>About me</div>
                 <div>Contact</div>
             </div>
